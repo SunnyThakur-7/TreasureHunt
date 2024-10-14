14944ec5d3fe35e5de5d4d3f9df6b31e6c31729a
SunnyThakur-7 <140975604+SunnyThakur-7@users.noreply.github.com>
2024-10-14 12:28:00 +0530

    Add files via upload

---
+++ src/main/GamePanel.java
@@ -0,0 +1,148 @@
+package main;
+import javax.swing.JPanel;
+
+import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
+
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    final int originalTileSize = 16;
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize*maxScreenCol;
+    public final int screenHeigth = tileSize*maxScreenRow;
+
+    //World Settings
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+   
+
+    
+    int FPS = 60;
+
+    public TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler(this);
+    Sound music = new Sound();
+    Sound se = new Sound();
+
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+    Thread gameThread;
+
+    //Entity and Object
+    public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    
+    // GAME STATE
+    public int gameState;
+    public final int playState = 1;
+    public final int pauseState = 2;
+
+    
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeigth));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void setUpGame(){
+        aSetter.setObject();
+        playMusic(0);
+        stopMusic();
+        gameState = playState;
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+        
+    }
+    @Override
+    public void run(){
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        int timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime-lastTime);
+            lastTime = currentTime;
+            
+            if(delta >=1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >=1000000000){
+                System.out.println("FPS: "+drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update(){
+        
+        if (gameState == playState)
+        {
+            player.update();
+        }
+        if (gameState == pauseState)
+        {
+            //Nothing
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        //TILE
+        tileM.draw(g2);
+
+        //OBJECT
+        for(int i = 0;i< obj.length ; i++){
+            if(obj[i] != null){
+                obj[i].draw(g2, this);
+            }
+        }
+
+        //PLAYER
+        player.draw(g2);
+
+        //UI
+        ui.draw(g2);
+        g2.dispose();
+    }
+    public void playMusic(int i){
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i){
+      se.setFile(i);
+      se.play();
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame window  = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setUpGame();
+        gamePanel.startGameThread();
+    }
+}
+
+//Guys keep it Updated
+//Added
+//Video No. = 15(Part #13) Completed.
